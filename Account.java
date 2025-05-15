ad440e7b13442668bb81188d21c266d9e5092ac1
Shunmugasundaram <129269793+sundarst@users.noreply.github.com>
2025-05-15 22:14:57 +0530

    Account.java

--- Account.java
+++ Account.java
@@ -1,18 +1,24 @@
-public class Account
-{
-	String no_borrowed_books;
-	String no_reserved_book;
-	String no_return_books;
-	String no_lost_book;
-	String fine_amount;
-	
-	public void claculate_fine()
-	{
-		
-	}
-	public static void main(String [] args)
-	{
-		
-	}
-	
+public class Account {
+
+    int noBorrowedBooks;
+    int noReservedBooks;
+    int noReturnedBooks;
+    int noLostBooks;
+    double fineAmount;
+
+    public void calculateFine() {
+        // Assuming Rs.50 fine for each lost book
+        fineAmount = noLostBooks * 50;
+        System.out.println("Total Fine: ₹" + fineAmount);
+    }
+
+    public static void main(String[] args) {
+        Account acc = new Account();
+        acc.noBorrowedBooks = 5;
+        acc.noReservedBooks = 2;
+        acc.noReturnedBooks = 4;
+        acc.noLostBooks = 1;
+
+        acc.calculateFine();
+    }
 }
