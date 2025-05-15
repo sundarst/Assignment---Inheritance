de01f4605158183974af3ea30be7970cff22320c
Shunmugasundaram <129269793+sundarst@users.noreply.github.com>
2025-05-15 22:34:01 +0530

    Staf.java

--- Staf.java
+++ Staf.java
@@ -1,10 +1,13 @@
-Public class Staf
-{
-	String dep;
-	
-	public static void main(String []args)
+public class Staff {
+    String dep;
+
+	Staff(String dep)
 	{
-		
+		this.dep=dep;
 	}
 
+    public static void main(String[] args) {
+        Staff staffMember = new Staff("Human Resources");
+        System.out.println("Department: " + staffMember.dep);
+    }
 }
