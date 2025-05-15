2fb815a9b6f1e03360e7782cc73a5d69eb128a50
Shunmugasundaram <129269793+sundarst@users.noreply.github.com>
2025-05-15 22:37:24 +0530

    Student.java

--- Student.java
+++ Student.java
@@ -1,10 +1,12 @@
-public class Student
-{
-String class;
-public static void main(String []args)
-{
+public class Student {
+    String className;
+  Student(String className)
+  {
+    this.className=className;
+  }
 
-
-}
-
+    public static void main(String[] args) {
+        Student student = new Student("10th Grade");
+        System.out.println("Class: " + student.className);
+    }
 }
