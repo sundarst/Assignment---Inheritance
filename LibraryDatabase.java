a87d16209478acf89d2a7e8778b52d840e47cbe7
Shunmugasundaram <129269793+sundarst@users.noreply.github.com>
2025-05-15 22:22:33 +0530

    LibraryDatabase.java

--- LibraryDatabase.java
+++ LibraryDatabase.java
@@ -1,30 +1,32 @@
-public class LibraryDatabase
-{
-	String list_of_book;
-	
-	public void add()
-	{
-		
-	}
-	public void Delete()
-	{
-		
-	}
-	public void update()
-	{
-		
-	}
-	public void display()
-	{
-		
-	}
-	public void search()
-	{
-		
-	}
-	public static void main(String [] args)
-	{
-		
-	}
-	
+public class LibraryDatabase {
+    String list_of_book;
+
+    public void add() {
+        System.out.println("Adding a book to the database");
+    }
+
+    public void Delete() {
+        System.out.println("Deleting a book from the database");
+    }
+
+    public void update() {
+        System.out.println("Updating book details in the database");
+    }
+
+    public void display() {
+        System.out.println("Displaying all books in the database");
+    }
+
+    public void search() {
+        System.out.println("Searching for a book in the database");
+    }
+
+    public static void main(String[] args) {
+        LibraryDatabase db = new LibraryDatabase();
+        db.add();
+        db.Delete();
+        db.update();
+        db.display();
+        db.search();
+    }
 }
